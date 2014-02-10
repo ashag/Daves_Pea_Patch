dab0135346d624c0dca06bbb8a24731077a8bda2
Sue White <sue@mailworks.org>
2014-02-10 11:37:36 -0800

    initial commit; adds images

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PeaPatch::Application.config.secret_key_base = '1c6592f053fa6a3dfc9372213584c521af37c5a69bb2507ac6bf4c747981a0305889cca11e0a5279067261a8107bae4adbd381ad3f47e7c43369d414db8a92bf'
