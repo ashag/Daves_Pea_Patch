6cab9175366f028b3de919e3d30efdc78c241f68
Agolveo <agolveo@gmail.com>
2014-02-10 14:17:42 -0800

    created user, tool, and user_tool models

---
+++ app/models/user_tool.rb
@@ -0,0 +1,2 @@
+class UserTool < ActiveRecord::Base
+end
---
+++ spec/factories/tools.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :tool do
+    name "MyString"
+    qty "MyString"
+    integer "MyString"
+  end
+end
