c326ace2053c3000b87bf2837cae8e2d595d81bb
Agolveo <agolveo@gmail.com>
2014-02-11 22:00:45 -0800

    factory girl setup

--- spec/factories/tools.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :tool do
-    name "MyString"
-    qty "MyString"
-    integer "MyString"
-  end
-end
