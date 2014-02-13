affef9d1588f4c4e39bb36dd4de3399e5b23975e
Sue White <sue@mailworks.org>
2014-02-13 15:35:12 -0800

    forecast partial renders at /forecast, root page breaks

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -2,7 +2,6 @@ class WeatherController < ApplicationController
 
   def forecast
     w_api = Wunderground.new
-    @weather_hash = w_api.forecast_for("Seattle, WA")
-
+    @weather_hash = w_api.forecast_for("South Africa", "Cape Town", lang: "EN")
   end
 end
