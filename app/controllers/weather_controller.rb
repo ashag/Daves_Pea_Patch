750a05a15eb34451bac7e1e6acaf1d2e125ad14b
Sue White <sue@mailworks.org>
2014-02-13 09:08:46 -0800

    weather renders partial in tab

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -3,6 +3,6 @@ class WeatherController < ApplicationController
   def forecast
     w_api = Wunderground.new
     @weather_hash = w_api.forecast_for("Seattle, WA")
-    render 'forecast'
+
   end
 end
