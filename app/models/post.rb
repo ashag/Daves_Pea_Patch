ed2ff0c93dee137464abf91a1e9ab1c6d2fe7094
Sue White <sue@mailworks.org>
2014-02-20 14:19:51 -0800

    calendar renders partial views

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,6 @@ class Post < ActiveRecord::Base
     users = User.where(news: true)
     email_users = users.map { |user| user.emails }
 
-    Pony.mail(to: email_users, from: "adaBugFree13@gmail.com", subject: "New Blog Post - #{self.title}", body: )
+    Pony.mail(to: email_users, from: "adaBugFree13@gmail.com", subject: "New Blog Post - #{self.title}", body: "Hello World" )
   end
 end
